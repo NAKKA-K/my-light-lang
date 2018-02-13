190b4801842b72086a408fd6eb4f2bf233b14ba0
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-02-13 15:53:41 +0000

    Add operation and function eval

--- src/lightang.c
+++ src/lightang.c
@@ -2,25 +2,32 @@
 #include<ctype.h>
 #include<stdarg.h>
 #include<stdlib.h>
+#include<string.h> // silent warning from 'strchr' ...
 
 // Prototype
-static int eval();
 static void spaceSkip();
 static void error(char *fmt, ...);
+static void expect(char c);
+static void readUntil(char untilChar, char *buf);
+static int evalFunction(char *code, int arg);
+static int eval(int arg);
 int getNameric();
+int getCalcResult(int arg);
 
 
 static char *p;
+static char func[26][100];
 
 
 int main(int argc, char **argv){
     p = argv[1];
     while(*p){
-        printf("%d\n", eval());
+        printf("%d\n", eval(0));
     }
 
     return 0;
 }
+
 
 static void spaceSkip(){
     while(isspace(*p))
@@ -35,8 +42,28 @@ static void error(char *fmt, ...){
     exit(1);
 }
 
+static void expect(char c){
+    if(*p != c)
+        error("%c expected: %s", p);
+    p++;
+}
 
-static int eval(){
+static void readUntil(char untilChar, char *buf){
+    while(*p != untilChar)
+        *buf++ = *p++;
+    p++;
+    *buf = '\n';
+}
+
+static int evalFunction(char *code, int arg){
+    char *originPointer = p;
+    p = code; // Replace code to method
+    int val = eval(arg);
+    p = originPointer; // Reassign pointer
+    return val;
+}
+
+static int eval(int arg){
     spaceSkip();
 
     // Nameric
@@ -44,16 +71,53 @@ static int eval(){
         return getNameric();
     }
 
-    if(*p++ == '+'){
-        return eval() + eval();
+    // Operate
+    if(strchr("+-*/", *p)){
+        return getCalcResult(arg);
+    }
+
+    
+    if(*p == '.'){
+        p++;
+        return arg;
+    }
+
+    if(*p >= 'A' && *p <= 'Z' && *(p + 1) == '['){
+        char name = *p;
+        p += 2;
+        readUntil(']', func[name - 'A']);
+        return eval(arg);
+    }
+
+    if(*p >= 'A' && *p <= 'Z' && *(p + 1) == '('){
+        char name = *p;
+        p += 2;
+        int newArg = eval(arg);
+        expect(')');
+        return evalFunction(func[name - 'A'], newArg);
     }
 
     error("invalid character: %c", *p);
 }
+
 
 int getNameric(){
     int val = *p++ - '0';
     while(isdigit(*p))
         val = val * 10 + (*p++ - '0');
     return val;
+}
+
+int getCalcResult(int arg){
+    // Confirm execution order in formula
+    char op = *p++;
+    int x = eval(arg);
+    int y = eval(arg);
+
+    switch(op){
+    case '+': return x + y;
+    case '-': return x - y;
+    case '*': return x * y;
+    case '/': return x / y;
+    }
 }
