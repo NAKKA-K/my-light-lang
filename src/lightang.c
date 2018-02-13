434df9eccdc69382fc4c6a4315b53254f98d2b38
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-02-13 00:39:22 +0000

    Add input nameric to return

--- src/lightang.c
+++ src/lightang.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
+#include<ctype.h>
+#include<stdarg.h>
+#include<stdlib.h>
 
 // Prototype
 static int eval();
+static void spaceSkip();
+static void error(char *fmt, ...);
+int getNameric();
 
 
 static char *p;
+
 
 int main(int argc, char **argv){
     p = argv[1];
@@ -15,7 +22,34 @@ int main(int argc, char **argv){
     return 0;
 }
 
+static void spaceSkip(){
+    while(isspace(*p))
+        p++;
+}
+
+static void error(char *fmt, ...){
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+
 static int eval(){
-    p++;
-    return 0;
+    spaceSkip();
+
+    // Nameric
+    if(isdigit(*p)){
+        return getNameric();
+    }
+
+    error("invalid character: %c", *p);
+}
+
+int getNameric(){
+    int val = *p++ - '0';
+    while(isdigit(*p))
+        val = val * 10 + (*p++ - '0');
+    return val;
 }
